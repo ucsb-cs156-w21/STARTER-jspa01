f1c76aab7ef4530615e827c44137679388f08268
Scott Chow <tehchowster@gmail.com>
2020-09-23 18:39:04 -0700

    sc - add test coverage for rational helpers

--- src/rationals/Helpers.test.js
+++ src/rationals/Helpers.test.js
@@ -5,11 +5,21 @@ describe("gcd tests", () => {
     const result = gcd(12, 3);
     expect(result).toBe(3);
   });
+
+  test("gcd throws an error when an incorrect type is passed for either argument", () => {
+    expect(() => gcd({}, 2)).toThrow();
+    expect(() => gcd(1, {})).toThrow();
+  });
 });
 
 describe("lcm tests", () => {
   test("lcm of 3 and 5 is 15", () => {
     const result = lcm(3, 5);
     expect(result).toBe(15);
+  });
+
+  test("lcm throws an error when an incorrect type is passed for either argument", () => {
+    expect(() => lcm({}, 2)).toThrow();
+    expect(() => lcm(1, {})).toThrow();
   });
 });
