1f0e485cc2739c590091133f9b21175a07734ed0
Scott Chow <tehchowster@gmail.com>
2020-09-24 22:03:50 -0700

    sc - reconstruct app with router, move app test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./App.css";
 import Calculator from "./pages/Calculator";
+import { Container } from "react-bootstrap";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Calculator />
+      <Container className="flex-grow-1 mt-5">
+        <Switch>
+          <Route exact path="/" component={Calculator} />
+        </Switch>
+      </Container>
     </div>
   );
 }
