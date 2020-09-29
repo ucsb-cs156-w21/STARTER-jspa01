08a0ea959c372f88225868ad411585a417021791
Scott Chow <tehchowster@gmail.com>
2020-09-28 17:20:14 -0700

    sc - remove logic to get to starter code

--- javascript/src/main/pages/Multiply.jsx
+++ javascript/src/main/pages/Multiply.jsx
@@ -1,16 +1,7 @@
 import React from "react";
-import { multiplyRationalsFromUserInput } from "main/utils/RationalHelpers";
-import Calculator from "main/components/Calculator";
 
 const Multiply = () => {
-  const props = {
-    calculateFromUserInput: multiplyRationalsFromUserInput,
-    title: "Multiply",
-    subtitle:
-      "Fill in the numerator and denominator of two rational numbers (fractions) below, then click the Calculate button to see the result",
-    sign: "*",
-  };
-  return <Calculator {...props} />;
+  return <div>Mult</div>;
 };
 
 export default Multiply;
--- javascript/src/main/utils/RationalHelpers.js
+++ javascript/src/main/utils/RationalHelpers.js
@@ -4,13 +4,7 @@ const parseRationalsFromUserInput = (userInput) => {
   const rationals = userInput.map((value) => {
     const numerator = parseInt(value.numerator);
     const denominator = parseInt(value.denominator);
-    if (isNaN(numerator)) {
-      throw new Error(`Invalid numerator recieved: got "${value.numerator}"`);
-    } else if (isNaN(denominator)) {
-      throw new Error(
-        `Invalid denominator recieved: got "${value.denominator}"`
-      );
-    }
+
     return new Rational(numerator, denominator);
   });
 
@@ -28,35 +22,15 @@ const addRationalsFromUserInput = (userInput) => {
 };
 
 const subtractRationalsFromUserInput = (userInput) => {
-  const [firstRational, secondRational] = parseRationalsFromUserInput(
-    userInput
-  );
-
-  const result = Rational.subtract(firstRational, secondRational);
-  return result;
+  return 42;
 };
 
 const multiplyRationalsFromUserInput = (userInput) => {
-  const [firstRational, secondRational] = parseRationalsFromUserInput(
-    userInput
-  );
-
-  const result = Rational.multiply(firstRational, secondRational);
-  return result;
+  return 42;
 };
 
 const divideRationalsFromUserInput = (userInput) => {
-  const [firstRational, secondRational] = parseRationalsFromUserInput(
-    userInput
-  );
-  if (secondRational.numerator === 0) {
-    throw new Error(
-      "Invalid numerator recieved for the second rational during division: 0."
-    );
-  }
-
-  const result = Rational.quotient(firstRational, secondRational);
-  return result;
+  return 42;
 };
 
 export {
